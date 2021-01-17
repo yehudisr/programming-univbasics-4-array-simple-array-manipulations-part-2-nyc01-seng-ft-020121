1c7115998aff819a096d50a909d063e8ff730c03
yehudis raitman <yehudisr@gmail.com>
2021-01-17 21:11:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,11 @@ end
 def using_flatten(array)
   array.flatten 
 end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at[integer]
+end
